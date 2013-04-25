5a75301c5454b599b7614c8659a73028bc674b92
Saurabh Aggarwal <SaurabhAgg@gmail.com>
2013-04-24 22:43:30 -0500

    AppConstant relative file path.

--- src/AppConstants.java
+++ src/AppConstants.java
@@ -5,11 +5,14 @@ public class AppConstants
 {
 	
 	// TAXONOMY Related
-	public static final String TAXONOMY_SOURCE_XML = "B:\\Codebase\\ws_cs784\\TweeTrend\\src\\Taxonomy_Movies.xml";
+	//public static final String TAXONOMY_SOURCE_XML = "B:\\Codebase\\ws_cs784\\TweeTrend\\src\\Taxonomy_Movies.xml";
+	public static final String TAXONOMY_SOURCE_XML = "C:\\Tweets\\Taxonomy_Movies.xml";
+	
 	public static final String TAXONOMY_DELIMITER_STRING = "\\s+|:|,|\\.|&|-";
-
+	
 	// TWEETS Related
-	public static final String TWITTER_DATA_FILE = "C:\\Users\\Prakhar\\Desktop\\Spring 2013\\cs784\\ProjectWork\\TwitterData\\twitter_20110731-12.log";
+	//public static final String TWITTER_DATA_FILE = "C:\\Users\\Prakhar\\Desktop\\Spring 2013\\cs784\\ProjectWork\\TwitterData\\twitter_20110731-12.log";
+	public static final String TWITTER_DATA_FILE = "C:\\Tweets\\twitter_20110731-12.log";
 	public static final String TWEET_DELIMITER_STRING = "\\s+|:|,|\\.|&|-|\\?";
 	
 	// DEBUGGING Related
