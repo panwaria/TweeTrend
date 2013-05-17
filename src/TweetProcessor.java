b6aa3a15851eaa8fda439210336b6750cf9eb0b0
Prakhar Panwaria <panwaria@wisc.edu>
2013-05-17 16:41:31 -0500

    Changing Movie Cast Score from 0.25 to 0.1. Plus updating the Project Report

--- src/TweetProcessor.java
+++ src/TweetProcessor.java
@@ -342,7 +342,7 @@ public class TweetProcessor
 	
 	private void extractMentionsOfMovieCasts(ArrayList<String> tokens, boolean isHash)
 	{
-		double SCORE = isHash ? 0.5 : 0.25;
+		double SCORE = isHash ? 0.2 : 0.1;
 		for(String token : tokens)
 		{
 //			System.out.println(token);
